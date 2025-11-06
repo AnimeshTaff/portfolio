a9424d941a400a648bad9b798f2054f227563002
Animeshkumar5 <animeshbca003@gmail.com>
2025-11-06 23:11:37 +0530

    fixing error

--- src/pages/WebDesign.jsx
+++ src/pages/WebDesign.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { X } from "lucide-react";
-import ContactUs from "../components/Contact.jsx";
+
 
 
 const tools = [
